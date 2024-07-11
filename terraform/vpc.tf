64810395cb4bd1f6d37e36fddfc60eafe96c672a
leovct <l009.vincent@gmail.com>
2024-07-11 13:19:31 +0200

    chore: update resource names

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,13 +5,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-zero-prover-vpc"
+  name                    = "${var.prefix}-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-zero-prover-subnet"
+  name          = "${var.prefix}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/16"
