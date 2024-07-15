0de2c230b5a529348e03b5eb6db1de938d34c402
leovct <l009.vincent@gmail.com>
2024-07-15 14:59:34 +0200

    fix: test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
-# List of witness numbers to process
-witness_numbers=(0000 0001 0034 0036 0128)
+# List of witness to process.
+witnesses=(/tmp/test-data/432.erc721.witness.json /tmp/test-data/512.eoa.witness.json /tmp/test-data/512.erc20.witness.json)
 
-total_files=${#witness_numbers[@]}
+total_files=${#witnesses[@]}
 successful_proofs=0
 failed_proofs=0
 
 start_time=$(date +%s)
 echo "Starting script at $(date)"
 
-# Function to run the command for a single witness
+# Function to run to prove a single block, given a witness.
 run_proof() {
-  local n=$1
-  local witness="data/witness-$n.json"
-  local output="data/leader-$n.out"
-
+  local witness="$1"
+  echo -e "\n> Generating proof for $witness..."
   env RUST_BACKTRACE=full RUST_LOG=debug leader \
     --runtime=amqp \
     --amqp-uri=amqp://guest:guest@test-rabbitmq-cluster.zero.svc.cluster.local:5672 \
-    stdio < "$witness" &> "$output"
+    stdio < "$witness"
 
   if [ $? -eq 0 ]; then
-    echo "Successfully proved witness-$n.json"
+    echo "> Successfully proved $witness"
     ((successful_proofs++))
   else
-    echo "Failed to prove witness-$n.json"
+    echo "> Failed to prove $witness"
     ((failed_proofs++))
   fi
 }
 
-export -f run_proof
-
-# Run the proofs in parallel using GNU Parallel
-parallel --jobs 0 --wait run_proof ::: "${witness_numbers[@]}"
+# Generate the proofs using a for loop.
+for witness in "${witnesses[@]}"; do
+  run_proof "$witness"
+done
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
 
-echo "Script completed in $duration seconds"
+echo -e "\nScript completed in $duration seconds"
 echo "Total files processed: $total_files"
 echo "Successful runs: $successful_proofs"
 echo "Failed runs: $failed_proofs"
