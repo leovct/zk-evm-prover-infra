287cb3f72b278ac0a95ae4176132e27537f47896
leovct <l009.vincent@gmail.com>
2024-07-24 18:51:07 +0200

    chore: nit

--- tools/rabbitmq/rabbitmq.go
+++ tools/rabbitmq/rabbitmq.go
@@ -16,7 +16,7 @@ const (
 	// password  = "Ynumtrr4N8kLogVtGWSLsMJwxayCMcpb"
 	password = "guest"
 	hostname = "localhost"
-	//hostname  = "test-rabbitmq-cluster.zero.svc.cluster.local"
+	//hostname  = "rabbitmq-cluster.zero.svc.cluster.local"
 	port      = 5672
 	queueName = "hello"
 	message   = "test-msg"
